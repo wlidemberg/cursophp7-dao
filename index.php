28228ea12ed7e122e80ace8401a6087731cb954a
wlidemberg <automatizei@gmail.com>
2019-03-19 11:17:27 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 require_once ("Config.php");
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega um lista de usuários buscando pelo login
+//$search = Usuario::search("b");
+//echo json_encode($search);
+
+//Carrega  um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("berg", "wl150680");
+echo $usuario;
